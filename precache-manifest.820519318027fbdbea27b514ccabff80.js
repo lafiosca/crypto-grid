c88fec6d23efb867f937695f8766929de302a3f1
Joe Lafiosca <joe@lafiosca.com>
2019-03-21 17:17:42 -0400

    Updates

---
+++ precache-manifest.820519318027fbdbea27b514ccabff80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e90d4e72636b129858b",
+    "url": "/crypto-grid/static/js/runtime~main.57110f2e.js"
+  },
+  {
+    "revision": "0005fe2b1a89341ec1be",
+    "url": "/crypto-grid/static/js/main.4a805e05.chunk.js"
+  },
+  {
+    "revision": "3b405addd3fdc1cf257d",
+    "url": "/crypto-grid/static/js/2.473ccb39.chunk.js"
+  },
+  {
+    "revision": "0005fe2b1a89341ec1be",
+    "url": "/crypto-grid/static/css/main.833a0f95.chunk.css"
+  },
+  {
+    "revision": "215f26f4b399355a2a4617361dfc6826",
+    "url": "/crypto-grid/index.html"
+  }
+];
